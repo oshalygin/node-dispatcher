8dee75ca96d54ba0b6fdfece69847a211c50c60d
Oleg Shalygin <oshalygin@gmail.com>
2017-10-23 21:13:11 -0700

    Wrapping up solution

--- events.js
+++ events.js
@@ -1,7 +1,42 @@
-// function Events() {}
 class Events {
-    on = eventName => {
-        return eventName;
+  events = new Map();
+
+  on = (eventName, callback, scope) => {
+    const { events } = this;
+
+    if (events.has(eventName)) {
+      const callbacks = events.get(eventName);
+      callbacks.set(callback.toString(), [callback, scope ? scope : null]);
+      events.set(eventName, callbacks);
+    } else {
+      const callbacks = new Map();
+      callbacks.set(callback.toString(), [callback, scope ? scope : null]);
+      events.set(eventName, callbacks);
     }
+  };
+
+  trigger = (eventName, ...args) => {
+    const { events } = this;
+
+    const callbacks = events.get(eventName);
+    for (const callback of callbacks.values()) {
+      callback[0].call(callback[1] ? callback[1] : this, args);
+    }
+  };
+
+  off = (eventName, callback) => {
+    const { events } = this;
+
+    const callbacks = events.get(eventName);
+    //eslint-disable-next-line no-undefined
+    if (callback === undefined) {
+      callbacks.clear();
+      events.set(eventName, callbacks);
+      return;
+    }
+
+    callbacks.delete(callback.toString());
+    events.set(eventName, callbacks);
+  };
 }
 module.exports = Events;
--- events.spec.js
+++ events.spec.js
@@ -1,35 +1,36 @@
-/* eslint-env jasmine */
+/* eslint-disable prefer-arrow-callback */
 
-const Events = require('./events.js');
+import Events from './events';
 
-describe('Event Dispatcher', function() {
+import sinon from 'sinon';
+
+describe('Event Dispatcher', () => {
   let events;
-  beforeEach(function() {
+  beforeEach(() => {
     events = new Events();
   });
 
-  it('is a function', function() {
+  it('is a function', () => {
     expect(typeof Events).toBe('function');
   });
 
-  it('can register a callback', function() {
-    events.on('foo', function() {});
+  it('can register a callback', () => {
+    events.on('foo', () => {});
   });
 
-  it('can register a callback with a scope', function() {
-    events.on('foo', function() {}, this);
+  it('can register a callback with a scope', () => {
+    events.on('foo', () => {}, this);
   });
 
-  it('can trigger an event', function() {
-    const listener = jasmine.createSpy('listener');
+  it('can trigger an event', () => {
+    const listener = sinon.spy();
     events.on('foo', listener);
 
     events.trigger('foo');
-
-    expect(listener).toHaveBeenCalled();
+    expect(listener.called).toEqual(true);
   });
 
-  it('can trigger an event registered with scope', function(done) {
+  it('can trigger an event registered with scope', done => {
     const scope = {};
 
     events.on(
@@ -44,72 +45,73 @@ describe('Event Dispatcher', function() {
     events.trigger('foo');
   });
 
-  it('can trigger an event with arguments', function() {
-    const listener = jasmine.createSpy('listener');
+  it('can trigger an event with arguments', () => {
+    const listener = sinon.spy();
     events.on('foo', listener);
 
     events.trigger('foo', 'bar', 'baz');
-
-    expect(listener).toHaveBeenCalledWith('bar', 'baz');
+    expect(listener.calledWith(['bar', 'baz'])).toEqual(true);
   });
 
-  it('can trigger multiple callbacks on an event', function() {
-    const listener1 = jasmine.createSpy('listener1');
-    const listener2 = jasmine.createSpy('listener2');
-    events.on('foo', listener1);
-    events.on('foo', listener2);
+  it('can trigger multiple callbacks on an event', () => {
+    const listenerOne = sinon.spy(function firstCallBack() {});
+    const listenerTwo = sinon.spy(function secondCallback() {});
+
+    events.on('foo', listenerOne);
+    events.on('foo', listenerTwo);
 
     events.trigger('foo');
 
-    expect(listener1).toHaveBeenCalled();
-    expect(listener2).toHaveBeenCalled();
+    expect(listenerOne.called).toEqual(true);
+    expect(listenerTwo.called).toEqual(true);
   });
 
-  it('can remove callbacks from an event', function() {
-    const listener = jasmine.createSpy('listener');
+  it('can remove callbacks from an event', () => {
+    const listener = sinon.spy();
     events.on('foo', listener);
 
     events.trigger('foo');
-    expect(listener.calls.length).toBe(1);
+    expect(listener.callCount).toEqual(1);
 
     events.off('foo');
     events.trigger('foo');
-    expect(listener.calls.length).toBe(1);
+    expect(listener.callCount).toEqual(1);
   });
 
-  it('can remove a specific callback from an event', function() {
-    const listener1 = jasmine.createSpy('listener1');
-    const listener2 = jasmine.createSpy('listener2');
-    events.on('foo', listener1);
-    events.on('foo', listener2);
+  it('can remove a specific callback from an event', () => {
+    const listenerOne = sinon.spy(function firstCallBack() {});
+    const listenerTwo = sinon.spy(function secondCallback() {});
+
+    events.on('foo', listenerOne);
+    events.on('foo', listenerTwo);
 
     events.trigger('foo');
-    expect(listener1.calls.length).toEqual(1);
-    expect(listener2.calls.length).toEqual(1);
+    expect(listenerOne.callCount).toEqual(1);
+    expect(listenerTwo.callCount).toEqual(1);
 
-    events.off('foo', listener1);
+    events.off('foo', listenerOne);
     events.trigger('foo');
-    expect(listener1.calls.length).toEqual(1);
-    expect(listener2.calls.length).toEqual(2);
+    expect(listenerOne.callCount).toEqual(1);
+    expect(listenerTwo.callCount).toEqual(2);
   });
 
-  it('can remove a specific callback registered with a scope from an event', function() {
-    const listener1 = jasmine.createSpy('listener1');
-    const listener2 = jasmine.createSpy('listener2');
+  it('can remove a specific callback registered with a scope from an event', () => {
+    const listenerOne = sinon.spy(function firstCallBack() {});
+    const listenerTwo = sinon.spy(function secondCallback() {});
 
     const scope1 = {};
     const scope2 = {};
 
-    events.on('foo', listener1, scope1);
-    events.on('foo', listener2, scope2);
+    events.on('foo', listenerOne, scope1);
+    events.on('foo', listenerTwo, scope2);
 
     events.trigger('foo');
-    expect(listener1.calls.length).toEqual(1);
-    expect(listener2.calls.length).toEqual(1);
+    expect(listenerOne.callCount).toEqual(1);
+    expect(listenerTwo.callCount).toEqual(1);
 
-    events.off('foo', listener1);
+    events.off('foo', listenerOne);
     events.trigger('foo');
-    expect(listener1.calls.length).toEqual(1);
-    expect(listener2.calls.length).toEqual(2);
+    expect(listenerOne.callCount).toEqual(1);
+    expect(listenerTwo.callCount).toEqual(2);
   });
 });
